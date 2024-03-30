26f86ee82c490867b8a1668a81dc0dc65dbcd698
trungshin <trungviet6969@gmail.com>
2024-03-30 14:42:51 +0700

    Init Project

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8000
+
+app.get('/', function (req, res) {
+    res.send('<h1>Hello World</h1>')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Server started at http://${hostname}:${port}/`)
+})
