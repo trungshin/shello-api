88b1c3e488ef4ca5a18f1c53a732e60335879951
trungshin <trungviet6969@gmail.com>
2024-09-20 23:26:24 +0700

    Fix environment.js

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,7 +3,7 @@ import 'dotenv/config.js'
 export const env = {
   MONGODB_URI: process.env.MONGODB_URI,
   DATABASE_NAME: process.env.DATABASE_NAME,
-  LOCAL_HOST: process.env.HOST,
-  LOCAL_PORT: process.env.PORT,
+  LOCAL_HOST: process.env.LOCAL_HOST,
+  LOCAL_PORT: process.env.LOCAL_PORT,
   BUILD_MODE: process.env.BUILD_MODE
 }
